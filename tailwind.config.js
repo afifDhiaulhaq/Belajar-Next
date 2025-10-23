7c324a98af5748e4b0e2d063b2a8d3e877adc171
afifDhiaulhaq <85942500+afifDhiaulhaq@users.noreply.github.com>
2025-10-23 09:28:49 +0700

    add file

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                "primary": "#f1f2f4",
+                "background-light": "#f7f7f7",
+                "background-dark": "#18191a",
+            },
+            fontFamily: {
+                "display": ["Inter"]
+            },
+            borderRadius: { "DEFAULT": "0.25rem", "lg": "0.5rem", "xl": "0.75rem", "full": "9999px" },
+        },
+    },
+}
