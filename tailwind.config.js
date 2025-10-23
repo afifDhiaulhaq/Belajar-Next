7896f6e88fd5365cc683ec46eccbd90e2d94363f
afifDhiaulhaq <85942500+afifDhiaulhaq@users.noreply.github.com>
2025-10-23 03:20:44 +0000

    upadte

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./components/**/*.{js,ts,jsx,tsx}",
         "./app/**/*.{js,ts,jsx,tsx}",
+        "./component/**/*.{js,ts,jsx,tsx}",
         "./*.{js,ts,jsx,tsx}",
     ],
     theme: {
